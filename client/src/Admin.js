f550351253fc0678ca9412e282e267cc0c736d8e
Darren <darrenxu1994@gmail.com>
2018-12-20 17:50:12 +1100

    adding messages

--- client/src/Admin.js
+++ client/src/Admin.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import FormSnippet from './components/FormSnippet'
+import { postSection } from './api/section'
 
 class Admin extends Component {
     state = {
-        title: null,
+        name: null,
         subSections: []
     }
     handleChange = (event) => {
@@ -14,26 +15,34 @@ class Admin extends Component {
         event.preventDefault();
 
         console.log(this.state)
+        postSection(this.state)
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
 
     submitSkillForm = (values) => {
         let newSS = this.state.subSections
-        newSS.push({"skill": values.skill, "description": values.description})
-        this.setState({subSections: newSS})
+        newSS.push({ "skill": values.skill, "description": values.description })
+        this.setState({ subSections: newSS })
     }
     render() {
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
+                {/* This should be dropdown instead */}
                     <label>
                         Name:
-                        <input type="text" name="title" value={this.state.value} onChange={this.handleChange} />
+                        <input type="text" name="name" value={this.state.value} onChange={this.handleChange} />
                     </label>
                     <input type="submit" value="Submit" />
                 </form>
-                <FormSnippet submitSkillForm = {this.submitSkillForm} />
+                <FormSnippet submitSkillForm={this.submitSkillForm} />
 
-                { this.state.subSections.map(entry=> {
+                {this.state.subSections.map(entry => {
                     return <div key={Math.random().toString(36).substr(2, 9)}><h3>{entry.skill}</h3><p>{entry.description}</p></div>
                 })}
             </div>
--- client/src/api/section.js
+++ client/src/api/section.js
@@ -4,5 +4,12 @@ module.exports = {
     getSection: (name) => {
         return axios.get(`http://localhost:5000/api/section/${name}`)
 
+    },
+    postSection: (obj) => {
+        return axios.post('http://localhost:5000/api/section', {
+            name: obj.name,
+            subSections: obj.subSections
+          })
+          
     }
 }
