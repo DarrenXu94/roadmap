50de6fda546bbbf30398e0d7e931a58d6a4983fe
Darren <darrenxu1994@gmail.com>
2018-12-21 16:43:53 +1100

    adding and updating new section

--- routes/api/Section.js
+++ routes/api/Section.js
@@ -9,29 +9,30 @@ const Section = require('../../models/Section');
 // @access  Public
 router.get('/', (req, res) => {
     Section.find()
-    .then(items => res.json(items));
+        .then(items => res.json(items));
 });
 
 router.get('/:name', (req, res) => {
-    Section.find({name: req.params.name})
-    .then(items => res.json(items));
+    Section.find({ name: req.params.name })
+        .then(items => res.json(items));
 });
 
 // @route   POST api/items
-// @desc    Create An Item
+// @desc    Should check if item exists based on name and replace if necessary
 // @access  Public
 router.post('/', (req, res) => {
-  const newItem = new Section({
-      name: req.body.name,
-      subSections: req.body.subSections
-  });
-  newItem.save().then(item => res.json(item));
+    const newItem = {
+        name: req.body.name,
+        subSections: req.body.subSections
+    }
+    Section.findOneAndUpdate({ name: req.body.name }, newItem, { upsert: true })
+        .then(items => res.json(items));
 });
 
 router.delete('/:id', (req, res) => {
     Section.findById(req.params.id)
-    .then(item => item.remove().then(() => res.json({ success: true })))
-    .catch(err => res.status(404).json({ success: false }));
+        .then(item => item.remove().then(() => res.json({ success: true })))
+        .catch(err => res.status(404).json({ success: false }));
 });
 
 module.exports = router;
