8e2265770a2e324f44e1b2908c3f971969343e44
Darren <darrenxu1994@gmail.com>
2018-12-22 14:58:10 +1100

    show active

--- client/src/components/Section.js
+++ client/src/components/Section.js
@@ -6,15 +6,22 @@ class Section extends Component {
         console.log(this.props.data)
     }
     state = {
+        skillSelected: "",
         description: ""
     }
     showDescription = (e) => {
-        this.setState({description: e.description})
+        let toShow = (e.description === this.state.description) ? "" : e.description
+        this.setState({description: toShow})
+
+        let toShowActive = (e.skill === this.state.skillSelected) ? "" : e.skill
+        this.setState({skillSelected: toShowActive})
+
     }
     render() {
         let { name, subSections } = this.props.data
         let { className } = this.props
         let buttonColor = (className === 'bg-light') ? 'btn-light' : 'btn-dark'
+        
         return (
             <div className={`${className} sec`} >
                 <h1>{ name }</h1>
@@ -24,8 +31,9 @@ class Section extends Component {
                     return <div key={ skill }><h3>{ skill }</h3><p>{ description }</p></div>
                 })}
                 { subSections.map(sub => {
+                    let active = (this.state.skillSelected === sub.skill) ? "btn-active" : ""
                     /*eslint-disable */
-                    return <a onClick={()=>this.showDescription(sub)} className={`subSection-button ${buttonColor}`} key={sub.skill}>{ sub.skill }</a>
+                    return <a onClick={()=>this.showDescription(sub)} className={`subSection-button ${buttonColor} ${active}`} key={sub.skill}>{ sub.skill }</a>
                     /*eslint-enable */
                 })}
                 </div>
